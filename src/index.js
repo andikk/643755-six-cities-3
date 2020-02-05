216f615a5e1b65013b4dc4680caf1f1fa5a020fe
andikk <andikk@mail.ru>
2020-02-05 09:42:54 +0300

    module1-task2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  OFFERS_COUNT: 312
+};
+
+ReactDOM.render(
+    <App offersCount={Settings.OFFERS_COUNT}/>,
+    document.getElementById(`root`)
+);
