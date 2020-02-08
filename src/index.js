7feba7a2bc71b2769be45513d79f4f45f0b59747
andikk <andikk@mail.ru>
2020-02-08 12:58:10 +0300

    PropTypes added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const Settings = {
   OFFERS_COUNT: 312
 };
 
+const places = [`Place1`, `Place2`, `Place3`, `Place4`];
+
 ReactDOM.render(
-    <App offersCount={Settings.OFFERS_COUNT}/>,
+    <App offersCount={Settings.OFFERS_COUNT} places={places}/>,
     document.getElementById(`root`)
 );
