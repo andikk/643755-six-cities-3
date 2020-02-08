4683e2d5d14ba5bb04f9e4b28fe988b4c0f1c445
andikk <andikk@mail.ru>
2020-02-08 13:02:21 +0300

    PropTypes fix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
+
   const {offersCount, places} = props;
 
   return (
     <Main offersCount={offersCount} places={places}/>
   );
+};
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+  places: PropTypes.array.isRequired
 };
 
 export default App;
