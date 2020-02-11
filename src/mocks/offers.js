d1de8df652d6e10323c7c6957f4baef1c4c688f6
andikk <andikk@mail.ru>
2020-02-11 23:11:16 +0300

    mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    mark: `Premium`,
+    src: `img/apartment-01.jpg`,
+    price: 120,
+    rating: 80,
+    name: `Name1`,
+    type: `Apartment`
+  },
+  {
+    mark: `NePremium`,
+    src: `img/apartment-01.jpg`,
+    price: 220,
+    rating: 40,
+    name: `Name2`,
+    type: `NeApartment`
+  },
+  {
+    mark: `SuperPremium`,
+    src: `img/apartment-01.jpg`,
+    price: 420,
+    rating: 140,
+    name: `Name3`,
+    type: `SuperNeApartment`
+  },
+];
