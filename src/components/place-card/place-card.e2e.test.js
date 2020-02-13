d592fc13e7938753f92f940e3e3c2b635c02003b
andikk <andikk@mail.ru>
2020-02-13 23:17:34 +0300

    test place-card fix

--- src/components/place-card/place-card.e2e.test.js
+++ src/components/place-card/place-card.e2e.test.js
@@ -2,52 +2,28 @@ import React from "react";
 import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import PlaceCard from "./place-card";
+import offers from "../../mocks/offers";
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
+const card = offers[1];
+const onHeaderClickHandler = jest.fn();
+const onCardHoverHandler = jest.fn();
+
+const placeCard = shallow(
+    <PlaceCard card={card} onCardHover={onCardHoverHandler} onHeaderClick={onHeaderClickHandler}/>
+);
 
 it(`Should mouse over PlaceCardMoved`, () => {
-  const onHeaderClickHandler = jest.fn();
-  const onCardHoverHandler = jest.fn();
-  const card = {
-    mark: `mark`,
-    src: `offer.src`,
-    price: 152,
-    rating: 80,
-    name: `offer.name`,
-    type: `offer.type`,
-  };
-
-
-  const placeCard = shallow(
-      <PlaceCard card={card} onCardHover={onCardHoverHandler} onHeaderClick={onHeaderClickHandler}/>
-  );
-
   placeCard.find(`.place-card`).simulate(`mouseleave`);
   expect(onCardHoverHandler).toHaveBeenLastCalledWith(null);
 
   placeCard.find(`.place-card`).simulate(`mouseenter`);
-  expect(onCardHoverHandler).toHaveBeenLastCalledWith(Object.getPrototypeOf(card));
+  expect(onCardHoverHandler).toHaveBeenLastCalledWith(card);
 });
 
 it(`Should PlaceCard header link be pressed`, () => {
-  const onHeaderClickHandler = jest.fn();
-  const onCardHoverHandler = jest.fn();
-  const card = {
-    mark: `mark`,
-    src: `offer.src`,
-    price: 152,
-    rating: 80,
-    name: `offer.name`,
-    type: `offer.type`,
-  };
-
-  const placeCard = shallow(
-      <PlaceCard card={card} onCardHover={onCardHoverHandler} onHeaderClick={onHeaderClickHandler}/>
-  );
-
   placeCard.find(`.place-card__name`).simulate(`click`);
   expect(onHeaderClickHandler).toHaveBeenCalledTimes(1);
-
 });
