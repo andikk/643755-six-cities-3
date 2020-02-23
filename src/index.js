76b9e9d6bde472d2d3384d30858801d8419640b0
andikk <andikk@mail.ru>
2020-02-23 18:19:13 +0300

    5.3, 5.4

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer';
+
 import offers from "./mocks/offers.js";
 
 const Settings = {
   OFFERS_COUNT: 312
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <App offersCount={Settings.OFFERS_COUNT}
-      offers={offers}
-    />,
+    <Provider store={store}>
+      <App/>
+    </Provider>,
     document.getElementById(`root`)
 );
+
+// <App offersCount={Settings.OFFERS_COUNT}
+//      offers={offers}
+// />
