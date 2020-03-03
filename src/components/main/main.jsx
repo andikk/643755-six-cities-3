0685b4b56c8e37bc05bf3c72e10e39d33f96a024
andikk <andikk@mail.ru>
2020-03-04 00:24:41 +0300

    MainEmpty component added

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,6 +6,7 @@ import withSelectedFilter from "../../hocs/with-selected-filter.jsx";
 import Map from "../map/map.jsx";
 import {connect} from "react-redux";
 import CitiesList from "../cities-list/cities-list.jsx";
+import MainEmpty from "../main-empty/main-empty.jsx";
 import {getCitiesListSelector, getCitySelector, getSortedOffersInCitySelector, getCoordinatesInCitySelector, getActiveFilter, getActiveOfferSelector} from "../../selectors.js";
 import {ActionCreator} from "../../reducer";
 
@@ -62,15 +63,7 @@ const Main = (props) => {
           }
 
           {(offersCount === 0) &&
-            <div className="cities__places-container cities__places-container--empty container">
-              <section className="cities__no-places">
-                <div className="cities__status-wrapper tabs__content">
-                  <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
-                </div>
-              </section>
-              <div className="cities__right-section"></div>
-            </div>
+            <MainEmpty/>
           }
 
         </div>
