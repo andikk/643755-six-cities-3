3a1c62792bee59bc479de8e119d9e5e5c361f713
andikk <andikk@mail.ru>
2020-03-07 10:19:17 +0300

    api.js

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const createAPI = () => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/six-cities`,
+    timeout: 5000,
+    withCredentials: true,
+  });
+  const onSuccess = (response) => response;
+  const onError = (error) => error;
+
+  api.interceptors.response.use(onSuccess, onError);
+
+  return api;
+};
+
+export default createAPI;
