85451002de5b530e0d302648b3bf2b09f187cd1f
andikk <andikk@mail.ru>
2020-03-14 16:54:51 +0300

    7-3 не делал. Это начало 8.1

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -48,7 +48,6 @@ const Main = (props) => {
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-
                   {(authorizationStatus === `AUTH`) &&
                     <a className="header__nav-link header__nav-link--profile" href="#">
                       <div className="header__avatar-wrapper user__avatar-wrapper"> </div>
@@ -61,7 +60,6 @@ const Main = (props) => {
                         <span className="header__login">Sign in</span>
                       </a>
                   }
-
                 </li>
               </ul>
             </nav>
