0096fb419e518c8095f1ef22c0d5a898929436f7
andikk <andikk@mail.ru>
2020-04-02 10:58:34 +0300

    map test

--- src/components/map/map.test.js
+++ src/components/map/map.test.js
@@ -1,16 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Map from "./map.jsx";
-import offers from "../../mocks/offers.js";
 
 it(`Should Map render correctly`, () => {
 
-  const coordinates = offers.map((offer) => (offer.coordinates));
-
   const tree = renderer
     .create(<Map
-      coordinates={coordinates}
-      className={`cities__map`}
+      city = {{name: `Cologne`, location: {"latitude": 50.938361, "longitude": 6.959974, "zoom": 13}}}
+      coordinates = {[[50.938361, 6.959974]]}
+      className = {`PropTypes.string`}
+      activeMarker = {[50.938361, 6.959974]}
     />)
     .toJSON();
 
