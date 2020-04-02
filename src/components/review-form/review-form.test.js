c60e07e2742cb4e17957ecf4649a7e2d15a32825
andikk <andikk@mail.ru>
2020-04-02 11:26:11 +0300

    review-form.test.js

---
+++ src/components/review-form/review-form.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import ReviewForm from "./review-form.jsx";
+
+it(`Should ReviewForm render correctly`, () => {
+
+  const tree = renderer
+    .create(<ReviewForm
+      offerId = {1}
+      handleSubmit = {() => {}}
+      rating = {1}
+      comment = {`PropTypes.string`}
+      invalid = {true}
+      submitting = {true}
+      onRatingChange = {() => {}}
+      onCommentChange = {() => {}}
+      onSubmit = {() => {}}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
