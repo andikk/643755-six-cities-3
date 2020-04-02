bc62e118ed954cb4a9e5899ff4a0d2d3b427a7f9
andikk <andikk@mail.ru>
2020-04-02 10:32:03 +0300

    favorites-test.js

--- src/components/cities-list/cities-list.test.js
+++ src/components/cities-list/cities-list.test.js
@@ -5,8 +5,8 @@ import CitiesList from "./cities-list.jsx";
 it(`Should CitiesList render correctly`, () => {
   const tree = renderer
     .create(<CitiesList
-      citiesList={[`Paris`, `Amsterdam`]}
-      city={`Paris`}
+      citiesList={[{name: `Cologne`, location: {"latitude": 50.938361, "longitude": 6.959974, "zoom": 13}}, {name: `Paris`, location: {"latitude": 50.938361, "longitude": 6.959974, "zoom": 13}}]}
+      city={{name: `Cologne`, location: {"latitude": 50.938361, "longitude": 6.959974, "zoom": 13}}}
       onCityClick = {() => {}}
     />)
     .toJSON();
